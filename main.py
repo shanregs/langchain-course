9c051f8e1728f09eb4fdcef5b1679cd05929e4e3
shanregs <shanregs@gmail.com>
2026-06-08 01:02:54 +0530

    Initial LangChain project setup

--- main.py
+++ main.py
@@ -1,13 +1,62 @@
+from itertools import chain
 import os
-
+from urllib import response
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI  
+from langchain_ollama import ChatOllama
+
+from langchain_core.prompts import PromptTemplate
+import requests
 
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
+    information = """
+                    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and former public official known for his leadership of Tesla and SpaceX. Musk has been the wealthiest person in the world since 2025; as of June 2026, Forbes estimates his net worth to be US$834 billion.
+
+                    Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
+
+                    In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, Tesla approved a pay package worth $1 trillion for Musk, which he is to receive over 10 years if he meets specific goals.
+
+                    Musk is a supporter of global far-right politics, figures, and political parties. He was the largest donor in the 2024 U.S. presidential election, where he supported Donald Trump. After Trump was inaugurated as president in January 2025, Musk served as Senior Advisor to the President and as the de facto head of the Department of Government Efficiency (DOGE). Shortly before a public feud with Trump, Musk left the Trump administration in May 2025 and returned to managing his companies.
+
+                    Musk's political activities, statements and views have made him a polarizing figure. He has been criticized for making unscientific and misleading statements, including spreading COVID-19 misinformation, promoting conspiracy theories, and affirming antisemitic, racist, and transphobic comments. His acquisition of Twitter was controversial due to a subsequent increase in hate speech and the spread of misinformation on the service, following his pledge to decrease censorship. His role in the second Trump administration attracted public backlash, particularly in response to DOGE.
+                    """
+    # print(information)
+    summary_template = """
+        given the details about a person {information} , I want you to create:
+        1. A short summary of the person in 1 sentences.
+        2. two interesting facts about them in 2 sentence less than 200 words
+    """
+    # print(summary_template)
+    chat_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOllama(model="gemma3:270m", temperature=0)
+    # chain = chat_template | llm
+    # response = chain.invoke(input={"information": information})
+   
+    # print(response.content)
+    # response = requests.get("http://localhost:11434/api/tags")
+    # print(response.json())
+
+    # 
+    # print(response.content)
+
+    llm2 = ChatOpenAI(temperature=0, model="gpt-5")
+    chain = chat_template | llm2
+    response = chain.invoke(input={"information": information})
+    print(response.content)
+
+    print("end of main")
 
 
+
+
+def restaurant():
+    print("Hello from restaurant!")
+    
 if __name__ == "__main__":
     main()
+    restaurant()
